d5299d27defd5efc4b698ba0226d83c897c92971
Begum Gunel <begumgunel@gmail.com>
2022-02-10 14:57:08 +0300

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Begüm Günel on 10.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
